aa40d49224f5e5fd2c2b7d3986fc60528cb8cf05
MrNwear <mr.y.neuer@gmail.com>
2024-01-18 09:09:22 +0200

    init list design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
+function Logo() {
+  return <h1>🏝️ FAR AWAY 🧳</h1>;
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => {
+          return (
+            <li key={item.id}>
+              <input type="checkbox" />
+              <span
+                style={{ textDecorationLine: item.packed && "line-through" }}
+              >
+                {item.quantity + " " + item.description}
+              </span>
+              <button>❌</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+function Form() {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState("");
 
-export default App;
+  function handleForm(e) {
+    e.preventDefault();
+    if (!description) {
+      alert("please enter task description");
+      return;
+    }
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleForm} style={{ display: "" }}>
+      <h3>What do you need for your trip ? 💙</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num}>{num}</option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🧳 You have X items on your list, and you aleardy packed X (x%)</em>;
+    </footer>
+  );
+}
